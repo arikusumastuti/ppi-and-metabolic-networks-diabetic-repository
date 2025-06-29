44a87fe6a23a4c5964d7c2e588a2d2be89a1f6e8
Ari Kusumastuti <arikusumastuti@gmail.com>
2025-06-29 10:02:56 +0700

    feat: sidebar layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,33 +1,36 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Link from "next/link";
-import { FaChevronDown } from "react-icons/fa6";
+import { FaChevronDown, FaPaperPlane } from "react-icons/fa6";
 
 export default function Sidebar() {
-    const [active, setActive] = useState(false);
-
     const [menu, setMenu] = useState([
         {
-            text: 'A',
+            text: 'Overview',
+            href: '/',
             icon: null,
             subs: []
         },
         {
-            text: 'B',
+            text: 'PPI',
+            href: null,
             icon: null,
             subs: [
                 {
                     text: 'B1',
+                    href: null,
                     icon: null,
                     subs: [
                         {
                             text: 'B11',
+                            href: '/',
                             icon: null,
                             subs: []
                         },
                         {
                             text: 'B12',
+                            href: '/',
                             icon: null,
                             subs: []
                         },
@@ -35,6 +38,39 @@ export default function Sidebar() {
                 },
                 {
                     text: 'B2',
+                    href: '/',
+                    icon: null,
+                    subs: []
+                },
+            ]
+        },
+        {
+            text: 'Metabolic Networks',
+            href: null,
+            icon: null,
+            subs: [
+                {
+                    text: 'B1',
+                    href: null,
+                    icon: null,
+                    subs: [
+                        {
+                            text: 'B11',
+                            href: '/',
+                            icon: null,
+                            subs: []
+                        },
+                        {
+                            text: 'B12',
+                            href: '/',
+                            icon: null,
+                            subs: []
+                        },
+                    ]
+                },
+                {
+                    text: 'B2',
+                    href: '/',
                     icon: null,
                     subs: []
                 },
@@ -45,31 +81,59 @@ export default function Sidebar() {
     return (
         <div className="w-1/6 h-screen overflow-auto bg-custom *:text-white">
             <Link href="/" className="flex p-4 mb-3 no-underline border-b-[1px]">
-                <span className="text-lg font-semibold">🧬 Met2Graph</span>
+                <span className="text-lg font-semibold border-s-2 ps-2">PPI & Metabolic Networks Repository</span>
             </Link>
-            <div className="flex flex-col px-4 py-2 space-y-4 *:text-[.9rem]">
-                <Link href="/">Overview</Link>
-                <p className="flex items-center justify-between cursor-pointer" onClick={() => setActive(!active)}>
-                    Metabolic Networks
-                    <FaChevronDown className={`${active ? '-rotate-180' : ''} duration-300`} />
-                </p>
-                <div className="flex flex-col ps-4 space-y-4">
-                    <p className="flex items-center justify-between cursor-pointer" onClick={() => setActive(!active)}>
-                        Metabolic Networks
-                        <FaChevronDown className={`${active ? '-rotate-180' : ''} duration-300`} />
-                    </p>
-                    <div className="flex flex-col ps-4 space-y-4">
-                        <Link href="/">Overview</Link>
-                        <Link href="/">Overview</Link>
-                    </div>
-                    <Link href="/">Overview</Link>
-                    <Link href="/">Overview</Link>
-                </div>
+            <div className="flex flex-col px-4 py-2 gap-4 *:text-[.9rem] *:mb-0 select-none">
+                {menu.map((item, index) => <SidebarItem key={index} {...item} />)}
             </div>
         </div>
     )
 }
 
-function SidebarItem(text, icon, subs) {
+function SidebarItem({text, href, icon, subs}) {
+    if (subs.length > 0) {
+        const [active, setActive] = useState(false);
+        const contentRef = useRef(null);
 
+        useEffect(() => {
+            if (contentRef.current) {
+                if (active) {
+                    contentRef.current.style.maxHeight = `1000px`;
+                    contentRef.current.style.visibility = 'visible';
+                    contentRef.current.style.opacity = '1';
+                } else {
+                    contentRef.current.style.maxHeight = '0px';
+                    contentRef.current.style.visibility = 'hidden';
+                    contentRef.current.style.opacity = '1';
+                }
+            }
+        }, [active]);
+
+        return (
+            <div>
+                <p className="flex items-center justify-between cursor-pointer" onClick={() => setActive(!active)}>
+                    {text}
+                    <FaChevronDown className={`${active ? '-rotate-180' : ''} duration-300`} />
+                </p>
+                <div
+                    ref={contentRef}
+                    className="overflow-hidden transition-all duration-300 ease-in-out"
+                    style={{ maxHeight: '0px', visibility: 'hidden', opacity: '0' }}
+                >
+                    <div className="flex flex-col ps-4 gap-4 mt-4">
+                        {subs.map((sub, index) => (
+                            <SidebarItem key={index} {...sub} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );   
+    } else {
+        return (
+            <Link className="flex items-center gap-2" href={href}>
+                {icon && <span className="text-lg">{icon}</span>}
+                {text}
+            </Link>
+        );
+    }
 }
