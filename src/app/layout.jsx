f57c64145371e939f46a1056b282ac5fc5cdf2f2
arikusumastuti <arikusumastuti@gmail.com>
2025-07-02 11:08:40 +0700

    refactor: use google fonts instead of next fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,6 @@
-import { Open_Sans } from "next/font/google"
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import SidebarMobile from "@/components/SidebarMobile";
-
-const OpenSans = Open_Sans({
-    variable: "--font-sans",
-})
 
 export const metadata = {
     title: "PPI & Metabolic Networks Repository for Diabetic",
@@ -16,7 +11,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
         <body
-            className={`${OpenSans.className} antialiased`}
+            className={`antialiased`}
         >
             <header></header>
             <main className="flex h-screen overflow-hidden">
