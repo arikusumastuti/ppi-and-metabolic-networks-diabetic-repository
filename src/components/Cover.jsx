d94c4f076e8452a83da0671ccb36e0f6a1d01193
arikusumastuti <arikusumastuti@gmail.com>
2025-07-16 11:18:29 +0700

    refactor: change cover title

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -9,7 +9,7 @@ export default function Cover () {
     return (
         <div className={`${show ? '' : '-translate-y-full'} absolute w-full h-screen bg-custom flex items-center justify-start z-50 duration-500 text-white px-4 md:px-16 xl:px-24 cursor-pointer`} onClick={() => setShow(!show)}>
             <div className="space-y-2">
-                <h1 className="text-xl md:text-3xl xl:text-5xl font-extrabold">Metabolic Networks Features</h1>
+                <h1 className="text-xl md:text-3xl xl:text-5xl font-extrabold">PPI & Metabolic Networks Diabetic Repository</h1>
                 <p className="md:text-xl xl:text-2xl">By Ari Kusumastuti</p>
             </div>
             <div className="absolute w-full left-0 bottom-0 flex justify-center py-12">
