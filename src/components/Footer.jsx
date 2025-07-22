3a9fd5d029dbbaa869ecbe80272e2f108543c225
arikusumastuti <arikusumastuti@gmail.com>
2025-07-22 09:26:19 +0700

    add: logo in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
+import Logo from "./Logo";
+
 export default function Footer () {
     return (
-        <footer className="w-full h-16 flex items-center px-8 text-sm">
-            <p>Copyright &copy; 2025 Ari Kusumastuti, Isa Irawan, Kistosil Fahim, Mario Rosario Guarracino, and Akhmad Roziqin</p>
+        <footer className="w-full flex flex-col items-center px-8 text-sm pb-4">
+            <div className="">
+                <Logo />
+            </div>
+            <div className="">
+                <p>Copyright &copy; 2025 Ari Kusumastuti, Isa Irawan, Kistosil Fahim, Mario Rosario Guarracino, and Akhmad Roziqin</p>
+            </div>
         </footer>
     )
 }
